247ac25ac0a9717e49bd458fa81d96e3fea00d50
malashkovv <malashkovv@gmail.com>
2019-09-18 13:19:11 +0300

    Added swagger for API docs

--- translation-api/app.py
+++ translation-api/app.py
@@ -1,6 +1,7 @@
 import requests
 
 from aiohttp import web
+from aiohttp_swagger import setup_swagger
 from translate import translator
 
 
@@ -14,6 +15,28 @@ def translate(text):
 
 
 async def handle(request):
+    """
+    ---
+    description: This end-point allows to translate from English to Russian using Google API services
+    tags:
+    - translation
+    produces:
+    - application/json
+    parameters:
+        - in: query
+          name: text
+          required: false
+          schema:
+            type: string
+          description: Text for translation
+    responses:
+        "200":
+            description: successful operation. Return translated text
+        "405":
+            description: invalid HTTP Method
+        "429":
+            description: Too many requests (Google API has restriction on how many calls you can do.)
+    """
     text = request.rel_url.query.get('text', "")
     translated_text = translate(text) if text else ""
     return web.json_response({"translation": translated_text})
@@ -22,6 +45,7 @@ async def handle(request):
 def init_func(argv=None):
     app = web.Application()
     app.add_routes([web.get('/translate', handle)])
+    setup_swagger(app)
     return app
 
 
