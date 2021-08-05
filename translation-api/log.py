906c9757318ef32dcb66f9e7f9d18df466955aa4
malashkovv <malashkovv@gmail.com>
2021-08-05 22:34:17 +0300

    Fixes for model and cicd

--- translation-api/log.py
+++ translation-api/log.py
@@ -1,9 +1,11 @@
 import logging
 
-logger = logging.getLogger('application')
+logger = logging.getLogger("application")
 logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s: %(message)s', "%y/%m/%d %H:%M:%S")
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s %(filename)s: %(message)s", "%y/%m/%d %H:%M:%S"
+)
 channel = logging.StreamHandler()
 
 # channel.setFormatter(formatter)
--- translation-api/model.py
+++ translation-api/model.py
@@ -1,11 +1,9 @@
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
 from log import logger
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 
 class Translator:
-
     def __init__(self, tokenizer, model):
         self.model = model
         self.tokenizer = tokenizer
@@ -13,7 +11,9 @@ class Translator:
     @classmethod
     def initialize(cls, code):
         tokenizer = AutoTokenizer.from_pretrained(f"Helsinki-NLP/opus-mt-{code}")
-        model = AutoModelForSeq2SeqLM.from_pretrained(f"Helsinki-NLP/opus-mt-{code}", torchscript=True)
+        model = AutoModelForSeq2SeqLM.from_pretrained(
+            f"Helsinki-NLP/opus-mt-{code}", torchscript=True
+        )
         return cls(tokenizer, model)
 
     @property
@@ -23,17 +23,17 @@ class Translator:
         else:
             device_type = "cpu"
             logger.warning("No cuda device is found! Using CPU instead. ")
-        return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        return device_type
 
     def translate(self, text):
-        tokenized_text = self.tokenizer([text], return_tensors='pt')
+        tokenized_text = self.tokenizer([text], return_tensors="pt")
         tokenized_text.to(self.device)
 
         translation = self.model.generate(**tokenized_text)
         return self.tokenizer.batch_decode(translation, skip_special_tokens=True)[0]
 
 
-translator = Translator.initialize('en-nl')
+translator = Translator.initialize("en-nl")
 
 
 def get_translator():
